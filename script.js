093f319a2ab10f32b7be2e98c59378063e5ab5d2
jurgengalicia <jurgen.galicia@gmail.com>
2022-06-30 00:37:42 -0700

    begin sidebar animations

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+
+//Button click active class
+function PageTransitions(){
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', () =>{
+            let currBtn = document.querySelectorAll('.active-btn');
+            currBtn[0].classList = currBtn[0].className.replace('active-btn',"")
+            this.className += 'active-btn'
+        })
+    }
+}
+
+PageTransitions();
