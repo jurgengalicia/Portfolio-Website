504cf6b6f8a5c929e1e594abbb6ae26b6f68fd8f
jurgengalicia <jurgen.galicia@gmail.com>
2022-07-08 01:52:15 -0700

    email page mock up

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
+const allPages = document.querySelector('.active-pages');
 
 
 //Button click active class
