59a8f107bc581db3c0999fc3af2ca6c4bfcc600c
jurgengalicia <jurgen.galicia@gmail.com>
2022-07-09 01:49:18 -0700

    theme toggle, contact page

--- script.js
+++ script.js
@@ -33,6 +33,16 @@ function PageTransitions(){
             element.classList.add('active')
         }
     })
+
+    //toggle theme button
+
+    const themeBtn = document.querySelector('.theme-btn')
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+
+    })
+
 }
 
 
