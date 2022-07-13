b468189487793ed580699a55b1e6c49c38466c53
jurgengalicia <jurgen.galicia@gmail.com>
2022-07-13 01:15:14 -0700

    firebase configuration

--- script.js
+++ script.js
@@ -4,6 +4,51 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 const allPages = document.querySelector('.active-pages');
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBf68oh1kAL6nXQgMMtONiHkB31wTDCfuA",
+  authDomain: "portfolio-contact-form-42b3e.firebaseapp.com",
+  projectId: "portfolio-contact-form-42b3e",
+  databaseURL: "https://portfolio-contact-form-42b3e-default-rtdb.firebaseio.com/",
+  storageBucket: "portfolio-contact-form-42b3e.appspot.com",
+  messagingSenderId: "249715377698",
+  appId: "1:249715377698:web:6d771391052f05fee8ec7b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//reference contactInfo collections
+let contactInfo = firebase.database().ref("contactForm");
+
+
+//contact form
+//listen for a submit
+function submitForm(e){
+    //e.preventDefault();
+    //get user input values
+
+    //let name = document.querySelector(".name").value;
+    let subject = document.querySelector(".subject").value;
+    let email = document.querySelector(".email").value;
+    let message = document.querySelector(".message").value;
+    console.log(name,subject,email,message)
+    saveContactInfo(name,subject,email,message);
+}
+
+//save info to firebase
+
+function saveContactInfo(name,subject,email,message){
+    let newContactInfo = contactInfo.push();
+
+    newContactInfo.set({
+        name: name,
+        subject: subject,
+        email: email,
+        message: message
+    });
+}
+
 
 //Button click active class
 function PageTransitions(){
@@ -46,4 +91,5 @@ function PageTransitions(){
 }
 
 
+document.querySelector(".contact-form").addEventListener(".main-btn",submitForm);
 PageTransitions();
